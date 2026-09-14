8efdb51f9cd06a8fa36b4a14c601f37893aa28ef
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2026-09-14 00:24:30 -0500

    Fixed the camera

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   player: { walkSpeed: 5, runSpeed: 10, acceleration: 10, deceleration: 13, rotationSpeed: 12, radius: .45, jumpSpeed: 8.5, gravity: 22, jumpBuffer: .12, spawn: [0, 8] as [number, number] },
-  camera: { distance: 17, height: 2, pitch: .57, minPitch: .2, maxPitch: 1.725, mouseSensitivity: .005, touchSensitivity: .007, smoothing: 7, fov: 48, near: .1, far: 420 },
+  camera: { distance: 17, height: 2, pitch: .57, minPitch: -.1, maxPitch: .57, mouseSensitivity: .005, touchSensitivity: .007, smoothing: 7, fov: 48, near: .1, far: 420 },
   companion: { followRadius: 7, chatRadius: 3.2, targetRadius: 5.25, speed: 5.8, roadSpeed: 6.8, acceleration: 7, deceleration: 10, rotationSpeed: 8, arrivalRadius: .4, guidedHold: 6, repathInterval: .6 },
   panels: { activationRadius: 8, deactivationRadius: 10, openDuration: .65, closeDuration: .3, verticalOffset: 7, rise: 3, scale: .014, width: 660, mobileWidth: 540, height: 510 },
   speech: { characterDelay: .028, spaceDelay: .045, commaDelay: .16, periodDelay: .32, lineBreakDelay: .38, holdDuration: 3, volume: .18, sampleOffset: .22, sampleDuration: .075, playbackRate: 1, playbackRateVariance: .12, minSoundInterval: .045 },
